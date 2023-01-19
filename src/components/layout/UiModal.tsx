cab6fe78e479c01bf8f8305e0bc958130c390777
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2023-01-19 13:43:39 +0200

    added shadow to modal

--- src/components/layout/UiModal.tsx
+++ src/components/layout/UiModal.tsx
@@ -16,17 +16,20 @@ const UiModal: React.FC<Props> = ({ show, closeHandler, children }) => {
       visible={show}
       onRequestClose={closeHandler}
     >
-      <View style={styles.root}>
-        <Card flex={1} style={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}>
+      <View style={[styles.root]}>
+        <Card flex={1} style={styles.transparentCard}>
           <Pressable
             style={styles.closeArea}
             onTouchStart={closeHandler}
           ></Pressable>
         </Card>
-        <Card
-          flex={2}
-          style={{ backgroundColor: LightColorsForest.A50, paddingBottom: 0 }}
-        >
+        <View style={[styles.shadowContainer]}>
+          <View style={[styles.shadow, { top: 0 }]} />
+          <View style={[styles.shadow, { top: 1 }]} />
+          <View style={[styles.shadow, { top: 2 }]} />
+          <View style={[styles.shadow, { top: 3 }]} />
+        </View>
+        <Card flex={2} style={styles.contentCard}>
           {children}
         </Card>
       </View>
@@ -47,6 +50,26 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     top: 0,
+    bottom: 0,
+  },
+  transparentCard: { backgroundColor: 'rgba(0, 0, 0, 0)', marginBottom: 0 },
+  contentCard: {
+    backgroundColor: LightColorsForest.A50,
+    marginBottom: 0,
+  },
+  shadowContainer: {
+    width: '100%',
+    height: 8,
+    position: 'relative',
+    bottom: -4,
+  },
+  shadow: {
+    width: '100%',
+    borderRadius: 2,
+    backgroundColor: 'rgba(0, 0, 0, 0.02)',
+    position: 'absolute',
+    left: 0,
+    right: 0,
     bottom: 0,
   },
 });
